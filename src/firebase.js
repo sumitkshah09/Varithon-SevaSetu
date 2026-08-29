723639aa80d54bb86e2b3ae6b679717f2f796a16
aarya745 <aaryamule710@gmail.com>
2026-08-29 10:19:50 +0530

    do

--- src/firebase.js
+++ src/firebase.js
@@ -3,33 +3,16 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  // your Firebase config
+  apiKey: "AIzaSyC53Ipe8wHEBb_wevpU3HdZbetCGAH81LY",
+  authDomain: "sevasetu-4b505.firebaseapp.com",
+  projectId: "sevasetu-4b505",
+  storageBucket: "sevasetu-4b505.firebasestorage.app",
+  messagingSenderId: "296506378377",
+  appId: "1:296506378377:web:7070312c0da7a85c705e58",
+  measurementId: "G-J6JM1E99XS"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-// for login 
-import {
-  signInWithEmailAndPassword
-} from "firebase/auth";
-
-import { auth } from "../firebase";
-const handleLogin = async () => {
-  try {
-    const userCredential =
-      await signInWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-
-    const user = userCredential.user;
-
-    console.log("Logged in:", user.uid);
-
-  } catch (error) {
-    console.error(error);
-  }
-};where 
